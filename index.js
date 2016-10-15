67915ade27838171ed78a102da2b214d592c59dc
Parth Doshi <doshi.parth9@gmail.com>
2016-10-15 13:34:27 -0400

    Finished backend script

--- index.js
+++ index.js
@@ -8,9 +8,45 @@ firebase.initializeApp({
 
 var db = firebase.database();
 var ref = db.ref();
+var courses = {};
+var index = 0;
+var length;
 
-request('http://api.purdue.io/odata/Courses', function (error, response, body) {
+request('http://api.purdue.io/odata/Subjects', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    ref.update({"Courses" : JSON.parse(body).value});
+    var subjects = JSON.parse(body).value;
+    length = subjects.length;
+    for(var subject in subjects){
+      addToObject(subjects[subject]);
+    }
+  } else if (error) {
+    console.log(error);
   }
 });
+
+
+var addToObject = function(currSubject){
+  request('http://api.purdue.io/odata/Courses?$filter=Subject/Abbreviation eq \'' + currSubject.Abbreviation + '\'', function (error, response, body) {
+    index++;
+    if (!error && response.statusCode == 200) {
+      courses[currSubject.Abbreviation] = {
+        "name" : currSubject.Name,
+        "courses" : JSON.parse(body).value
+      };
+      if (index == length){
+        ref.update({
+          "Courses" : courses
+        }, function(error){
+          if (error){
+            console.log(error);
+          } else {
+            console.log("finished");
+            process.exit();
+          }
+        });
+      }
+    } else if (error) {
+      console.log(error);
+    }
+  });
+}
