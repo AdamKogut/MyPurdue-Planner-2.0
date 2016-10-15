cce7211220e28e7ce75962a9e908a9fe7a265754
Parth Doshi <doshi.parth9@gmail.com>
2016-10-15 12:09:31 -0400

    firebase server testing finished

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var request = require('request');
 var firebase = require('firebase');
-request('http://api.purdue.io/odata/Courses?%24filter=Subject/Abbreviation%20eq%20%27CS%27&%24orderby=Number%20asc', function (error, response, body) {
+
+firebase.initializeApp({
+  serviceAccount: "Hello World-78086433a32a.json",
+  databaseURL: "https://hello-world-e96ef.firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref();
+
+request('http://api.purdue.io/odata/Courses', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(JSON.parse(body)); // Show the HTML for the Google homepage.
+    ref.update({"Courses" : JSON.parse(body).value});
   }
 });
